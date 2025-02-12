1b02fb464cf919a37ccef07c5eabbf2dbb20c9f3
americo macedo <americomascedo@gmail.com>
2025-02-11 21:35:10 -0300

    teste da atividade 4

--- atividade4.js
+++ atividade4.js
@@ -2,4 +2,7 @@ process.stdout.write('qual seu ano de nascimento? ')
 process.stdin.once ('data', function (data){
     var idade = data.toString().trim()
     console.log('voce tem ' + (2024-idade) + ' anos')
+
+
+    console.log('joquinho do arroz')
 })
